bf3432e30fbe3a79bbf4b52d9a329dcf4fedd8d3
Pavel Odintsov <pavel.odintsov@gmail.com>
2026-05-22 12:51:08 +0300

    Added new scope type

--- notify_json.py
+++ notify_json.py
@@ -22,7 +22,7 @@ parsed_details = json.loads(stdin_data)
 # Action could be: ban, unban, attack_status for BGP Blackhole mode and partial_block, partial_unblock for BGP Flow Spec mode
 action = parsed_details["action"]
 
-# Can be: per_host, hostgroup,network, per_remote_host
+# Can be: per_host, hostgroup,network, remote_host
 scope = parsed_details["alert_scope"]
 
 if scope == "" or scope == "host":
